e6b8a0b45877a27a9a9af6f8717ed1f459a167e2
David Dieu <daviddieu80@gmail.com>
2016-07-12 12:01:56 +0200

    template

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,28 @@
 module.exports = {
-  scope: '/',
+  scope: '',
   /**
    * Retourne la route pour l'enregistrer dans l'application
    * @param Application application
    */
   register: function (application) {
-    application.get(this.scope + '/', this.execute);
+    application.get(this.scope + '/', this.indexRoute);
+    application.get(this.scope + '/hello/:name', this.helloRoute);
   },
   /**
    * Execute et traite la route lors de son appel
    * @param Request request
    * @param Response response
    */
-  execute: function (request, response) {
-    response.render('index.twig', {'msg': 'twindex'});
+  indexRoute: function (request, response) {
+    response.render('index/index.twig', {'msg': 'twindex.'});
+  },
+  /**
+   * Execute et traite la route lors de son appel
+   * @param Request request
+   * @param Response response
+   */
+  helloRoute: function (request, response) {
+    var res = 'Hello ' + request.params.name;
+    response.render('index/index.twig', {'msg': res});
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ var expressLess = require('express-less');
 var twig = require('twig');
 var router = require('./core/router.js');
 var applicationConfig = require('./config/application.config.js')
+// New express application
 var application = express();
-router.ventilate(applicationConfig.controllers, application);
+// Config application (public folder, less preprocessor and twig)
 application.use(express.static('public'));
 application.use('/styles', expressLess(__dirname + '/less'))
 application.set('twig options', {strict_variable: false});
+// Register all controller in application.config
+router.ventilate(applicationConfig.controllers, application);
+// Starting server
 application.listen(80, function () {
-  console.log('Starting server trynode');
+  console.log('Starting server on 80');
 });
